3a4ea23378670ddda4de549c54934ee329096255
Weston Pace <weston.pace@gmail.com>
2018-04-13 22:28:06 -0500

    Finished off the card...somewhat

--- src/scridgets/components/card/card.component.ts
+++ src/scridgets/components/card/card.component.ts
@@ -9,34 +9,44 @@ export class CardComponent implements DoCheck {
 
   @ViewChild('double')
   private double?: ElementRef;
-  @ViewChild('single')
-  private single?: ElementRef;
-  private paddingLeftRight = 0;
-  fillChars = '';
+  @ViewChild('fullSpan')
+  private fullSpan?: ElementRef;
+  private paddingLeft = 0;
+  private paddingRight = 0;
+  private paddingTop = 0;
+  private paddingBottom = 0;
+  topLine = '';
+  lines: string[] = [];
 
   constructor(private host: ElementRef) {
 
   }
 
   getPaddingString() {
-    return '16px ' + this.paddingLeftRight + 'px';
+    return this.paddingTop + 'px ' + this.paddingRight + 'px ' + this.paddingBottom + 'px ' + this.paddingLeft + 'px';
   }
 
   ngDoCheck() {
     const doubleElement = (this.double as ElementRef).nativeElement as HTMLElement;
     const doubleBounds = doubleElement.getBoundingClientRect();
-    console.log('getBoundingClientRect().width=' + doubleElement.getBoundingClientRect().width);
-    const singleElement = (this.single as ElementRef).nativeElement as HTMLElement;
-    const singleBounds = singleElement.getBoundingClientRect();
-    console.log('singleBounds=' + singleBounds.width);
     const hostBounds = (this.host.nativeElement as HTMLElement).getBoundingClientRect();
     const widthInChars = hostBounds.width / (doubleBounds.width / 2);
-    console.log('widthInChars=' + widthInChars);
     const intWidthInChars = Math.floor(widthInChars);
-    console.log('intWidthInChars=' + intWidthInChars);
-    this.fillChars = '-'.repeat(intWidthInChars - 4) + '+';
-    console.log('Adding ' + this.fillChars.length + ' characters');
-    this.paddingLeftRight = doubleBounds.width / 2;
+    this.topLine = '-'.repeat(intWidthInChars - 4) + '+';
+    const numLines = hostBounds.height / doubleBounds.height;
+    const numLinesInt = Math.floor(numLines);
+    this.lines = [];
+    for(let i = 0; i < numLinesInt - 2; i++) {
+      this.lines.push('|' + ' '.repeat(intWidthInChars - 2) + '|');
+    }
+    this.lines.push('+' + '-'.repeat(intWidthInChars - 2) + '+');
+    this.paddingLeft = doubleBounds.width / 2;
+    const fullSpanElement = (this.fullSpan as ElementRef).nativeElement as HTMLElement;
+    const fullSpanWidth = fullSpanElement.getBoundingClientRect().width;
+    this.paddingRight = Math.max(this.paddingLeft, hostBounds.width - fullSpanWidth + this.paddingLeft);
+    this.paddingTop = doubleBounds.height;
+    const allLinesHeight = (this.lines.length + 1) * doubleBounds.height;
+    this.paddingBottom = Math.max(0, hostBounds.height - allLinesHeight) + this.paddingTop;
   }
 
 }
